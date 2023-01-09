597c51d18337ffc5f2453c161074283e7c642597
Chizer77 <97499725+Chizer77@users.noreply.github.com>
2023-01-09 23:09:19 +0800

    排版更改

---
+++ TEXQuotes.cpp
@@ -0,0 +1,22 @@
+//输入
+//输入由若干行包含偶数个双引号(" )字符的文本组成。输入由EOF字符结束。
+//输出
+//除了下述情况，输出的文本要和输入的一样：
+//每对双引号中的第一个 " 被替换为两个` 字符，并且，每对双引号中的第二个 " 被替换为两个 ' 字符。
+
+#include<iostream>
+using namespace std;
+const char p[][5] = {"``", "''"};
+
+int main() {
+    int k = 0;
+    char c;
+    while ((c = getchar()) != EOF) {
+        if(c == '\"') {
+            cout << p[k];
+            k = !k;
+        }
+        else putchar(c);
+    }
+    return 0;
+}
